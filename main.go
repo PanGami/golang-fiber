6512d772729c6140067b9d83865c0e8cff406643
PanGami <pancanugrahawicaksana@gmail.com>
2024-02-19 12:21:42 +0700

    initializers

--- main.go
+++ main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"log"
 
+	"github.com/PanGami/golang-fiber/initializers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
+
+func init() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatalln("Failed to load environment variables! \n", err.Error())
+	}
+	initializers.ConnectDB(&config)
+}
 
 func main() {
 	app := fiber.New()
